0efeb7527a1fee3b1382121812fe38e957ce62a1
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-10-29 14:51:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+
 <?php
-echo "Witaj świecie! Nazywam się Kacper Radecki i Kocham php";
-?>
+$servername = "mysql-radeckikacper.alwaysdata.net";
+$username = "217137";
+$password = "Ambulans#1";
+$dbname = "radeckikacper_mysql";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+$sql = "SELECT * FROM pracownicy";
+$wynik = mysqli_query($conn, $sql);
+
+
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik)){
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
