ea902406685299907629378b438b8abe0ec67708
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-11-05 14:25:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,15 +19,13 @@ $wynik = mysqli_query($conn, $sql);
     echo('<table border="1">');
     echo('<th>Imie</th><th>zarobki</th>');
 
-    while($wiersz=mysqli_fetch_assoc($wynik))
-    {
+    while($wiersz=mysqli_fetch_assoc($wynik)){
         echo('<tr>');
         echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
         echo('</tr>');
     }
 
     echo('</table>');
-    
 echo("<br>");
     
     
