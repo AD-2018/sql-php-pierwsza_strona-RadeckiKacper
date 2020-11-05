5419ae11110a199b587e22e500c4cd124d2861ad
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-11-05 14:49:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 <link rel="stylesheet" href="style.css">
     <h1>Kacper Radecki nr 10</h1>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-RadeckiKacper">Github</a>
     <div>
        <a href="orgPracownicy.php">organizacjaPracownicy.php</a><br/>
        <a href="funkcAgregujace.php">funkcjeAgregujace.php</a><br/>
