3fca42b11e896d1552d3c6fc9a83a8f0267137e8
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-11-05 14:29:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,69 +1,37 @@
-<!DOCTYPE html>
+</html>
+ <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="style.css">
+ <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+
+  <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-RadeckiKacper">Github</a>
+  
 <?php
 $servername = "mysql-radeckikacper.alwaysdata.net";
 $username = "217137";
 $password = "Niedlapsa1";
 $dbname = "radeckikacper_mysql";
 
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 $sql = "SELECT * FROM pracownicy";
- echo("Zadanie 1");
-    echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
-
-    while($wiersz=mysqli_fetch_assoc($wynik))
-    {
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
-echo("<br>");
-    
-    
-$sql = "SELECT * FROM pracownicy where imie like '%a'";
 $wynik = mysqli_query($conn, $sql);
-    echo("Zadanie 2");
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
     echo('<table border="1">');
     echo('<th>Imie</th><th>zarobki</th>');
 
-    while($wiersz=mysqli_fetch_assoc($wynik))
-    {
+    while($wiersz=mysqli_fetch_assoc($wynik)){
         echo('<tr>');
         echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
         echo('</tr>');
     }
 
     echo('</table>');
-    
-echo("<br>");
-  
-    
-$sql = "SELECT * FROM pracownicy where imie not like '%a'";
-$wynik = mysqli_query($conn, $sql);
-    echo("Zadanie 3");
-    echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
-
-    while($wiersz=mysqli_fetch_assoc($wynik))
-    {
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
-    
-echo("<br>");
-
-?>
-    
-</body>
-</html>
+ </body>
+ </html>
