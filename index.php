4f5be2309794b191e8e84010a43a9b07e15b0171
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-11-05 21:01:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,52 +10,6 @@
     </div>
 </head>
 <body>
-<?php
-$servername = "mysql-radeckikacper.alwaysdata.net";
-$username = "217137";
-$password = "Niedlapsa1";
-$dbname = "radeckikacper_mysql";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-$sql = "SELECT * FROM pracownicy";
-    echo("zadanie 1");
-$wynik = mysqli_query($conn, $sql);
-
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-    echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
-
-    while($wiersz=mysqli_fetch_assoc($wynik)){
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
-    
-    $sql = "SELECT * FROM pracownicy";
-    echo("zadanie 2");
-$wynik = mysqli_query($conn, $sql);
-
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-    echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
-
-    while($wiersz=mysqli_fetch_assoc($wynik)){
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
-  ?>
   </body>
   </html>
