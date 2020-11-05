f729274bf6001919c594047d477a5920c022efb1
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-11-05 14:28:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,13 +13,12 @@ $dbname = "radeckikacper_mysql";
 
 
 $sql = "SELECT * FROM pracownicy";
-$wynik = mysqli_query($conn, $sql);
-
-    echo("Zadanie 1");
+ echo("Zadanie 1");
     echo('<table border="1">');
     echo('<th>Imie</th><th>zarobki</th>');
 
-    while($wiersz=mysqli_fetch_assoc($wynik)){
+    while($wiersz=mysqli_fetch_assoc($wynik))
+    {
         echo('<tr>');
         echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
         echo('</tr>');
