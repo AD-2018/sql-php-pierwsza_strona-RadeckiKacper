3cc47d5d157ce6eb0507f2fc8b24949e2349f4c8
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-11-05 21:01:30 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -1,3 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+    <h1>Kacper Radecki nr 10</h1>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-RadeckiKacper">Github</a>
+   </head>
+<body>
 <?php
-echo("cos");
-?>
+$servername = "mysql-radeckikacper.alwaysdata.net";
+$username = "217137";
+$password = "Niedlapsa1";
+$dbname = "radeckikacper_mysql";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+$sql = "SELECT * FROM pracownicy";
+    echo("zadanie 1");
+$wynik = mysqli_query($conn, $sql);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik)){
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+    
+    $sql = "SELECT * FROM pracownicy";
+    echo("zadanie 2");
+$wynik = mysqli_query($conn, $sql);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik)){
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+  ?>
+  </body>
+  </html>
