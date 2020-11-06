e4ea4638e76b8fd0f41a98b5f568812d9e62355b
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-11-06 11:55:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -54,6 +53,27 @@ echo "Connected successfully";
     while($wiersz=mysqli_fetch_assoc($wynik)){
         echo('<tr>');
         echo('<td>'.$wiersz['imie'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
+    $sql = "SELECT * FROM pracownicy where zarobki<30";
+echo($sql);
+
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th><th>Dzial</th>');
+
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td><td>'.$row['dzial'].'</td>');
         echo('</tr>');
     }
 
