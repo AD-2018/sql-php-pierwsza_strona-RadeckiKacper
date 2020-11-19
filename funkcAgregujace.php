097a9ef7c1f7069022b4d99094b1cfb7a830ce69
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-11-19 14:04:43 +0100

    Update funkcAgregujace.php

--- funkcAgregujace.php
+++ funkcAgregujace.php
@@ -14,6 +14,9 @@
         <br/>
         <a href="data.php">Data i Czas</a></br>
     <br/>
+    <br/>
+        <a href="formularz.html">Formularz</a></br>
+    <br/>
     </div>
     <h1>Kacper Radecki nr 10</h1>
 </head>
