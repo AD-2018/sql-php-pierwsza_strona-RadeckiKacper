81f935c7749dee4b8a5af27eb607a84f074481bc
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-11-19 14:31:44 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -3,12 +3,13 @@ $servername = "mysql-radeckikacper.alwaysdata.net";
 $username = "217137";
 $password = "Niedlapsa1";
 $dbname = "radeckikacper_mysql";
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO pracownicy (null, imie, dzial, zarobki) 
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
        VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
 
 $conn->query($sql);
