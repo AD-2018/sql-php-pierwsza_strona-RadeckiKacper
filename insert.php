dfa78da667f957d1fd23c9f79c1419e3aace9faa
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-11-19 14:10:04 +0100

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "mysql-radeckikacper.alwaysdata.net";
+$username = "217137";
+$password = "Niedlapsa1";
+$dbname = "radeckikacper_mysql";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO Pracownik (null, imie, dzial, zarobki) 
+       VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
+
+$conn->query($sql);
+
+$conn->close();
+?>
