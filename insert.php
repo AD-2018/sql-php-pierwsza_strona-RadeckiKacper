40700ffe387aef5ff41f88d2ca09972f14501457
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-11-19 14:24:43 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -8,7 +8,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO Pracownik (null, imie, dzial, zarobki) 
+$sql = "INSERT INTO pracownicy (null, imie, dzial, zarobki) 
        VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
 
 $conn->query($sql);
