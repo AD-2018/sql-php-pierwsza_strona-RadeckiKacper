ab074463ea9b7c215caf33ccdd158457836347e9
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-11-26 13:57:54 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,6 +1,6 @@
 <?php
 echo("jestes w insert.php <br>");
-echo "<li>". $_POST['name'];
+echo "<li>". $_POST['imie'];
 echo "<li>". $_POST['dzial'];
 echo "<li>". $_POST['zarobki'];
 echo "<li>". $_POST['data_urodzenia'];
@@ -10,27 +10,19 @@ $username = "217137";
 $password = "Niedlapsa1";
 $dbname = "radeckikacper_mysql";
 
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-//definiujemy zapytanie $sql
-$sql = "INSERT INTO Pracownik (null, name, dzial,zarobki,data_urodzenia)
-	      VALUES (
-					null, 
-					$_POST['name'], 
-					$_POST['dzial'], 
-					$_POST['zarobki'],
-					$_POST['data_urodzenia']
-				";
-
-//wyświetlamy zapytanie $sql
-echo "<li>". $sql;
+$sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
+       VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
 
 if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
+  echo "<br>New record created successfully";
 } else {
+//informacja o ewentualnych błędach
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
