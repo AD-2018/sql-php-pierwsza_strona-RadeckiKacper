95ae1abb2fc19996b5e1796738aff53ace1099d2
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-11-27 11:35:13 +0100

    Update data.php

--- data.php
+++ data.php
@@ -17,6 +17,12 @@
     <br/>
         <a href="formularz.html">Formularz</a></br>
     <br/>
+<br/>
+        <a href="insert.php">insert</a></br>
+    <br/>
+<br/>
+        <a href="danedobazy.html">Dane do Bazy</a></br>
+    <br/>
     </div>
     <h1>Kacper Radecki nr 10</h1>
 </head>
