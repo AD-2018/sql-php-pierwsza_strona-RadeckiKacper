29ce289558318dce2e445cf271d3667d6ff72567
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-11-27 11:57:32 +0100

    Create lib.php

---
+++ lib.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "mysql-radeckikacper.alwaysdata.net";
+$username = "217137";
+$password = "Niedlapsa1";
+$dbname = "radeckikacper_mysql";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
