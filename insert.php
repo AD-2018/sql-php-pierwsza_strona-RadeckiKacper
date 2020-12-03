849ad364d9bd53288fd7724e2617aba71ccec65c
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-12-03 13:28:35 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -12,7 +12,7 @@ $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodze
 
 if ($conn->query($sql) === TRUE) {
   echo "<br>New record created successfully";
-  header('Location: https://php-pierwsze.herokuapp.com/danedobazy.html');
+  header('Location: https://php-pierwsze.herokuapp.com/danedobazy.php');
 } else {
 
   echo "Error: " . $sql . "<br>" . $conn->error;
