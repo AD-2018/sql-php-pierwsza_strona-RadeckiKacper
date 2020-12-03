e513f09406d273ba2644d5a1533c31980d4a24c2
RadeckiKacper <73647865+RadeckiKacper@users.noreply.github.com>
2020-12-03 13:24:35 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -5,24 +5,16 @@ echo "<li>". $_POST['dzial'];
 echo "<li>". $_POST['zarobki'];
 echo "<li>". $_POST['data_urodzenia'];
 
-$servername = "mysql-radeckikacper.alwaysdata.net";
-$username = "217137";
-$password = "Niedlapsa1";
-$dbname = "radeckikacper_mysql";
-
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+require_once "lib.php";
 
 $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
        VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
 
 if ($conn->query($sql) === TRUE) {
   echo "<br>New record created successfully";
+  header('Location: https://dyljoanna.herokuapp.com/danedobazy.php');
 } else {
-//informacja o ewentualnych błędach
+
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
