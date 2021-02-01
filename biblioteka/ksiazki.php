bf8b019724e9a11f41e7a416ddbea23c08fb498c
KacperRadecki <kacper.radecki1824@gmail.com>
2021-02-01 09:16:58 +0100

    fgn

--- biblioteka/ksiazki.php
+++ biblioteka/ksiazki.php
@@ -33,66 +33,69 @@
 <?php
    require_once("connect.php");
 
-echo ("<br>Autorzy<br>");
-$sql = "SELECT * FROM bibl_autor";
-  $result = mysqli_query($conn, $sql);
-    if ( $result) {
-        
-    } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-
-
-echo('<select name="Autor">');
-
-    while($row=mysqli_fetch_assoc($result)){
-        echo'<option value="'.$row['id_autor'].'">';
-        echo($row['autor']);
-        echo"</option>"; 
-    }
-echo('</select>');
-
-echo("<br>");
-
-echo ("<br>Tytuły<br>");
-$sql = "SELECT * FROM bibl_tytul";
-  $result = mysqli_query($conn, $sql);
-    if ( $result) {
-        echo "<li>ok";
-    } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-
-
-echo('<select name="Tytul">');
-
-    while($row=mysqli_fetch_assoc($result)){
-        echo'<option value="'.$row['id_tytul'].'">';
-        echo($row['tytul']);
-        echo"</option>"; 
-    }
-echo('</select>');
-
-echo("<br>");
-echo("<br>KSIĄŻKI I AUTORZY<br>");
-$sql = "SELECT id_book, autor, tytul FROM bibl_book, bibl_tytul, bibl_autor WHERE bibl_tytul.id_tytul = bibl_book.id_tytul AND bibl_autor.id_autor = bibl_book.id_autor";
-echo($sql);
-
-$result = mysqli_query($conn, $sql);
-if ( $result) {
-           } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-
-echo('<table border="1">');
-    echo('<th>ID BOOK</th><th>ID AUTOR</th><th>ID TYTUL</th>');
-
-    while($row=mysqli_fetch_assoc($result)){
-        echo('<tr>');
-        echo('<td>'.$row['id_book'].'</td><td>'.$row['autor'].'</td><td>'.$row['tytul'].'</td>');
-        echo('</tr>');
-    }
-
-echo('</table>');
+   echo ("<br>Autorzy<br>");
+   $sql = "SELECT * FROM bibl_autor";
+   echo ("<li>".$sql);
+     $result = mysqli_query($conn, $sql);
+       if ( $result) {
+           echo "<li>ok";
+       } else {
+         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+       }
+   
+   
+   echo('<select name="Autor">');
+   
+       while($row=mysqli_fetch_assoc($result)){
+           echo'<option value="'.$row['id_autor'].'">';
+           echo($row['autor']);
+           echo"</option>"; 
+       }
+   echo('</select>');
+   
+   echo("<br>");
+   
+   echo ("<br>Tytuły<br>");
+   $sql = "SELECT * FROM bibl_tytul";
+   echo ("<li>".$sql);
+     $result = mysqli_query($conn, $sql);
+       if ( $result) {
+           echo "<li>ok";
+       } else {
+         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+       }
+   
+   
+   echo('<select name="Tytul">');
+   
+       while($row=mysqli_fetch_assoc($result)){
+           echo'<option value="'.$row['id_tytul'].'">';
+           echo($row['tytul']);
+           echo"</option>"; 
+       }
+   echo('</select>');
+   
+   echo("<br>");
+   echo("<br>KSIĄŻKI I AUTORZY<br>");
+   $sql = "SELECT id_book, autor, tytul FROM bibl_book, bibl_tytul, bibl_autor WHERE bibl_tytul.id_tytul = bibl_book.id_tytul AND bibl_autor.id_autor = bibl_book.id_autor";
+   echo($sql);
+   
+   $result = mysqli_query($conn, $sql);
+   if ( $result) {
+           echo "<li>ok";
+       } else {
+         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+       }
+   
+   echo('<table border="1">');
+       echo('<th>ID BOOK</th><th>ID AUTOR</th><th>ID TYTUL</th>');
+   
+       while($row=mysqli_fetch_assoc($result)){
+           echo('<tr>');
+           echo('<td>'.$row['id_book'].'</td><td>'.$row['autor'].'</td><td>'.$row['tytul'].'</td>');
+           echo('</tr>');
+       }
+   
+   echo('</table>');
 
 ?>
