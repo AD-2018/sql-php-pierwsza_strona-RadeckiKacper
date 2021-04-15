3471494530aac2bd56c63571e12b1e3e8449759a
KacperRadecki <kacper.radecki1824@gmail.com>
2021-04-15 08:50:42 +0200

    asf

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "mysql-radeckikacper.alwaysdata.net";
-$username = "217137";
+$servername = "mysql-tather1824.alwaysdata.net";
+$username = "232125";
 $password = "Niedlapsa1";
-$dbname = "radeckikacper_mysql";
+$dbname = "tather1824_grid";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
