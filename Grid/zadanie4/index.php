d8fdbb0732388cf903bfe63e8622051d11282faa
KacperRadecki <kacper.radecki1824@gmail.com>
2021-04-15 10:48:30 +0200

    kl

--- Grid/zadanie4/index.php
+++ Grid/zadanie4/index.php
@@ -4,27 +4,89 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="style4.css">
     <title>CSS Grid</title>
-    <link rel="stylesheet" href="style4.css" />
   </head>
   <body>
     <div class="container">
       <header>
-        <h1>Zadanie 4</h1>
+      <div class="tabela1">
+      <?php
+    require_once("../../connect.php");
+    $sql = "SELECT * FROM fryzjerzy";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>id</th><th>fryzjer</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['fryzjer'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
+?>
+        </div>
       </header>
       <nav>
-        2
+      2
       </nav>
-
       <main>
-        3
+      <?php
+    require_once("../../connect.php");
+    $sql = "SELECT zakład.id, fryzjer, klient FROM fryzjerzy, zakład, klienci where fryzjerzy.id=zakład.fryzjer_id and klienci.id=zakład.klient_id";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>id</th><th>fryzjer</th><th>Uczeń</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['klient'].'</td><td>'.$row['fryzjer'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
+?>
       </main>
-      <aside>
-        5
- </aside>
       <footer>
-        4
+      4
       </footer>
+      <aside>
+      <div class="tabela1">
+      <?php
+    require_once("../../connect.php");
+    $sql = "SELECT * FROM klienci";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>Id</th><th>klient</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['klient'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
+?>
+        </div>
+      </aside>
     </div>
   </body>
 </html>
+
+
