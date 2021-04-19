58776d0754b38cd84fcfd44a01330a203e6ed387
KacperRadecki <kacper.radecki1824@gmail.com>
2021-04-19 10:09:55 +0200

    sdea

--- menu.php
+++ menu.php
@@ -1,25 +1,25 @@
 <div class="menu">
-<a class="menu_linki" href="https://github.com/AD-2018/sql-php-pierwsza_strona-RadeckiKacper">Github</a>
+<a class="menu_link" href="https://github.com/AD-2018/sql-php-pierwsza_strona-RadeckiKacper">Github</a>
 
 
     
-<a class="menu_linki" href="/pracownicy/orgPracownicy.php">organizacjaPracownicy.php</a>
+<a class="menu_link" href="/pracownicy/orgPracownicy.php">organizacjaPracownicy.php</a>
 
-<a class="menu_linki" href="/Grid/index.html">Grid</a>
-
-
-<a class="menu_linki" href="/pracownicy/funkcAgregujace.php">funkcjeAgregujace.php</a>
+<a class="menu_link" href="/Grid/index.html">Grid</a>
 
 
-<a class="menu_linki" href="/pracownicy/data.php">Data i Czas</a>
+<a class="menu_link" href="/pracownicy/funkcAgregujace.php">funkcjeAgregujace.php</a>
+
+
+<a class="menu_link" href="/pracownicy/data.php">Data i Czas</a>
     
 
-<a class="menu_linki" href="/pracownicy/insert.php">Dane do Bazy</a>
+<a class="menu_link" href="/pracownicy/insert.php">Dane do Bazy</a>
 
 
-<a class="menu_linki" href="/biblioteka/ksiazki.php">Książki</a>
+<a class="menu_link" href="/biblioteka/ksiazki.php">Książki</a>
 
 
-<a class="menu_linki" href="/flexbox/index.html">Flexbox</a>
+<a class="menu_link" href="/flexbox/index.html">Flexbox</a>
 
 </div>
