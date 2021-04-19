326d6ab2ab31b5bb84620babdc7f6d32fe0391a4
KacperRadecki <kacper.radecki1824@gmail.com>
2021-04-19 09:09:58 +0200

    df

---
+++ menu.php
@@ -0,0 +1,27 @@
+<a href="https://github.com/AD-2018/sql-php-pierwsza_strona-RadeckiKacper">Github</a></br>
+    <br/>
+    <br/>
+    <div>
+       <a href="/pracownicy/orgPracownicy.php">organizacjaPracownicy.php</a><br/>
+        <br/>
+        <div>
+       <a href="/Grid/index.html">Grid</a><br/>
+        <br/>
+        <br/>
+       <a href="/pracownicy/funkcAgregujace.php">funkcjeAgregujace.php</a><br/>
+        <br/>
+        <br/>
+        <a href="/pracownicy/data.php">Data i Czas</a></br>
+    <br/>
+    
+<br/>
+        <a href="/pracownicy/insert.php">Dane do Bazy</a></br>
+    <br/>
+<br/>
+        <a href="/biblioteka/ksiazki.php">Książki</a></br>
+    <br/>
+    <br/>
+        <a href="/flexbox/index.html">Flexbox</a></br>
+    <br/>
+
+    </div>
