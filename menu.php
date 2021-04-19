6900faf9a681c4edce229ceb511293be1557578d
KacperRadecki <kacper.radecki1824@gmail.com>
2021-04-19 09:27:55 +0200

    ewq

--- menu.php
+++ menu.php
@@ -1,29 +1,25 @@
 <link rel="stylesheet" href="style.css">
 <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-RadeckiKacper">Github</a></br>
-    <br/>
-    <br/>
-    <div>
-       <a href="/pracownicy/orgPracownicy.php">organizacjaPracownicy.php</a><br/>
-        <br/>
-        <br/>
-        <div>
-       <a href="/Grid/index.html">Grid</a><br/>
-        <br/>
-        <br/>
-       <a href="/pracownicy/funkcAgregujace.php">funkcjeAgregujace.php</a><br/>
-        <br/>
-        <br/>
-        <a href="/pracownicy/data.php">Data i Czas</a></br>
-    <br/>
-    
-<br/>
-        <a href="/pracownicy/insert.php">Dane do Bazy</a></br>
-    <br/>
-<br/>
-        <a href="/biblioteka/ksiazki.php">Książki</a></br>
-    <br/>
-    <br/>
-        <a href="/flexbox/index.html">Flexbox</a></br>
-    <br/>
+<
+<div>
+    <ul>
+<li><a href="/pracownicy/orgPracownicy.php">organizacjaPracownicy.php</a></li>
 
-    </div>
+<li><a href="/Grid/index.html">Grid</a></li>
+
+
+<li><a href="/pracownicy/funkcAgregujace.php">funkcjeAgregujace.php</a></li>
+
+
+<li><a href="/pracownicy/data.php">Data i Czas</a></br></li>
+    
+
+<li><a href="/pracownicy/insert.php">Dane do Bazy</a></br></li>
+
+
+<li><a href="/biblioteka/ksiazki.php">Książki</a></br></li>
+
+
+<li><a href="/flexbox/index.html">Flexbox</a></br></li>
+</ul>
+</div>
